bfb78422262eff1fa9701969d03fa684bf49e209
Carl Parker <carlparker.dev@icloud.com>
2024-08-21 10:56:57 -0700

    adjust index cards

--- src/components/CustomDocCard.tsx
+++ src/components/CustomDocCard.tsx
@@ -4,17 +4,19 @@ import { useCurrentSidebarCategory } from '@docusaurus/theme-common/internal';
 
 function CustomDocCard({ item }) {
   return (
-    <article className="col col--6 margin-bottom--lg">
-      <Link to={item.href} className="card padding--lg cardContainer">
+    <article className="col col--6 margin-bottom--lg ">
+      <div className="card padding--lg cardContainer">
         <h2 className="text--truncate cardTitle" title={item.label}>
-          {item.label}
+          <Link to={item.href} className="">
+            {item.label}
+          </Link>
         </h2>
         {item.description && (
           <p className="text--truncate cardDescription" title={item.description}>
             {item.description}
           </p>
         )}
-      </Link>
+      </div>
     </article>
   );
 }
@@ -22,7 +24,7 @@ function CustomDocCard({ item }) {
 export default function CustomDocCardList() {
   const category = useCurrentSidebarCategory();
   return (
-    <section className="row">
+    <section className="row cardGrid">
       {category.items.map((item, index) => (
         <CustomDocCard key={index} item={item} />
       ))}
